f9b94a2cf7095dd2e5ed24cdd7127c0d57317d11
stjeandan-swc <stjean.dan.dev@gmail.com>
2020-10-11 11:40:39 -0400

    Finished mapping city locations

--- assets/js/scenes/GameScene.js
+++ assets/js/scenes/GameScene.js
@@ -47,5 +47,33 @@ class GameScene extends Phaser.Scene {
         const Kinshasa = new Locations(this, 612, 520, 'temp');
         const Johannesburg = new Locations(this, 659, 603, 'temp');
         const Khartoum = new Locations(this, 665, 447, 'temp');
+
+        // black locations
+        const Algiers = new Locations(this, 587, 349, 'temp');
+        const Cairo = new Locations(this, 649, 365, 'temp');
+        const Isanbul = new Locations(this, 660, 294, 'temp');
+        const Moscow = new Locations(this, 727, 247, 'temp');
+        const Baghdad = new Locations(this, 719, 338, 'temp');
+        const Riyadh = new Locations(this, 730, 412, 'temp');
+        const Tehran = new Locations(this, 782, 286, 'temp');
+        const Karachi = new Locations(this, 799, 364, 'temp');
+        const Mumbia = new Locations(this, 807, 426, 'temp');
+        const Dehli = new Locations(this, 858, 341, 'temp');
+        const Chennai = new Locations(this, 872, 471, 'temp');
+        const Kolkata = new Locations(this, 918, 361, 'temp');
+
+        // red locations
+        const Beijing = new Locations(this, 964, 266, 'temp');
+        const Seoul = new Locations(this, 1037, 262, 'temp');
+        const Shanghai = new Locations(this, 969, 324, 'temp');
+        const Tokyo = new Locations(this, 1095, 296, 'temp');
+        const Osaka = new Locations(this, 1103, 358, 'temp');
+        const Taipei = new Locations(this, 1043, 383, 'temp');
+        const HongKong = new Locations(this, 977, 394, 'temp');
+        const Bangkok = new Locations(this, 929, 431, 'temp');
+        const Jakarta = new Locations(this, 929, 542, 'temp');
+        const HoChiMinhCity = new Locations(this, 980, 490, 'temp');
+        const Manila = new Locations(this, 1060, 485, 'temp');
+        const Sydney = new Locations(this, 1109, 655, 'temp');
     }
 }
